4e8bac2186192bb22d98ad909ce05ce9daa2ed1e
Deyaa Omer <61560614+Deyaa22@users.noreply.github.com>
2025-08-04 14:34:21 +0300

    Add Project

---
+++ Coroutine.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CoroutineNet
+{
+	/// <summary>
+	/// Provides an easy-to-use and lightweight coroutine timeline based on the C# iterator pattern with support for time-based delays.
+	/// Supports yielding <see cref="TimeSpan"/>, <see cref="int"/> (seconds), and <see cref="float"/> (seconds) for time delays.
+	/// </summary>
+	public sealed partial class Coroutine : IDisposable
+	{
+
+	}
+
+	// Public Interface
+	public sealed partial class Coroutine : IDisposable
+	{
+		public event Action Cancelled;
+
+		public event Action Terminated;
+
+		public static Coroutine StartCoroutine(Func<IEnumerable<object>> coroutine, Action TerminationCallback = null, Action CancellationCallback = null)
+		{
+			Coroutine newCoroutine = new Coroutine(coroutine);
+			newCoroutine.Terminated += TerminationCallback;
+			newCoroutine.Cancelled += CancellationCallback;
+			newCoroutine.StartASyncExecution(newCoroutine._method, newCoroutine._cancellationTokenSource);
+			return newCoroutine;
+		}
+
+		public void Cancel()
+		{
+			lock (_lock)
+			{
+				if (IsTerminated)
+					return;
+
+				IsCancelled = true;
+				if (!_isDisposed)
+					_cancellationTokenSource.Cancel();
+				OnCancel();
+				Terminate();
+			}
+		}
+
+		private void Terminate()
+		{
+			lock (_lock)
+			{
+				if (IsTerminated)
+					return;
+
+				IsTerminated = true;
+				OnTerminated();
+				Dispose();
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_isDisposed)
+				return;
+
+			if (!IsCancelled)
+			{
+				Cancel();
+				return;
+			}
+			_isDisposed = true;
+			_cancellationTokenSource.Dispose();
+			_cancellationTokenSource = null;
+			_method = null;
+			Cancelled = null;
+			Terminated = null;
+		}
+	}
+
+	// Functionality
+	public sealed partial class Coroutine : IDisposable
+	{
+		private Coroutine(Func<IEnumerable<object>> coroutine)
+		{
+			_method = coroutine;
+			_cancellationTokenSource = new CancellationTokenSource();
+		}
+
+		private async void StartASyncExecution(Func<IEnumerable<object>> coroutine, CancellationTokenSource source)
+		{
+			var enumerator = coroutine().GetEnumerator();
+			while (!IsCancelled && enumerator.MoveNext())
+			{
+				try
+				{
+					var i = enumerator.Current;
+					if (i is TimeSpan timespan)
+					{
+						await Task.Delay(timespan, source.Token);
+					}
+					else if (i is int seconds)
+					{
+						await Task.Delay(seconds * 1000, source.Token);
+					}
+					else if (i is float secs)
+					{
+						await Task.Delay((int)(secs * 1000), source.Token);
+					}
+				}
+				catch
+				{
+					break;
+				}
+			}
+			enumerator.Dispose();
+
+			if (!IsCancelled)
+				Terminate();
+		}
+
+		private void OnCancel()
+		{
+			Cancelled?.Invoke();
+		}
+
+		private void OnTerminated()
+		{
+			Terminated?.Invoke();
+		}
+	}
+
+	// Attributes & Properties
+	public sealed partial class Coroutine : IDisposable
+	{
+		private object _lock = new object();
+		private Func<IEnumerable<object>> _method;
+		private CancellationTokenSource _cancellationTokenSource;
+
+		private bool _isDisposed = false;
+		private bool _isTerminated = false;
+		public bool _isCancelled = false;
+		public bool IsCancelled { get { return _isCancelled || (!_isDisposed && _cancellationTokenSource.IsCancellationRequested); } private set { _isCancelled = value; } }
+		public bool IsTerminated { get { return _isTerminated || IsCancelled; } private set { _isTerminated = value; } }
+	}
+}
